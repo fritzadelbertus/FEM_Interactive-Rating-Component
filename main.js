6a3a1e0f61604da4aef5bd8e184584c82247a789
fritzadelbertus <107984452+fritzadelbertus@users.noreply.github.com>
2022-10-22 21:48:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const thankYouCard = document.querySelector('.thank-you-card')
 console.log(ratingForm);
 ratingForm.addEventListener('submit', (event)=> {
     event.preventDefault()
-    ratingCard.classList.add('hide-card')
-    const ratingValue = document.getElementById('rating-value')
-    ratingValue.innerText = document.querySelector('input[name="rating"]:checked').value
-    thankYouCard.classList.remove('hide-card')
+    const rating = document.querySelector('input[name="rating"]:checked')
+    if (rating == null) {
+        alert("Please input a rating.")
+    } else {
+        ratingCard.classList.add('hide-card')
+        const ratingValue = document.getElementById('rating-value')
+        ratingValue.innerText = rating.value
+        thankYouCard.classList.remove('hide-card')
+    }
 })
